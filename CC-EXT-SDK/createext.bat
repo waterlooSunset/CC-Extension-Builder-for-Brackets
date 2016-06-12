a3fa19f296922d4cef407bbd6ff524cfefec2952
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2016-06-12 14:49:32 -0400

    %2 in %20 drops com.project.plugin into file path

--- CC-EXT-SDK/createext.bat
+++ CC-EXT-SDK/createext.bat
@@ -1,5 +1,5 @@
 @echo off
 set sdk=%~dp0
-set dest=%PROGRAMFILES(x86)%\Common%20Files\Adobe\CEP\extensions\%2
+set dest=%PROGRAMFILES(x86)%\Common Files\Adobe\CEP\extensions\%2
 XCOPY "%sdk%\templates\%1" %dest% /D /E /C /R /I /K /Y >nul
 echo %dest%
