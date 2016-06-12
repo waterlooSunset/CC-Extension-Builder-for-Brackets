5fdb6a2eeaa98ce209a7a71c4d9f38cc008d4c95
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2016-06-12 15:26:31 -0400

    point direct to path in main.js

--- CC-EXT-SDK/deployext.bat
+++ CC-EXT-SDK/deployext.bat
@@ -1,4 +1,4 @@
 @echo off
-set dest=%PROGRAMFILES(x86)%\Common Files\Adobe\CEP\extensions
+set dest=%PROGRAMFILES(x86)%\Common Files\Adobe\CEP\extensions\%2
 XCOPY "%1" %dest% /D /E /C /R /I /K /Y >nul
 echo %dest%
