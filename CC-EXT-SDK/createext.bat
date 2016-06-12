9cb8ff45d88ae5bc0a0b2677723e24aafb1bda82
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2016-06-12 15:35:32 -0400

    point direct to path in main.js

--- CC-EXT-SDK/createext.bat
+++ CC-EXT-SDK/createext.bat
@@ -1,5 +1,6 @@
 @echo off
 set sdk=%~dp0
+mkdir C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\%2
 set dest=%PROGRAMFILES(x86)%\Common Files\Adobe\CEP\extensions\%2
 XCOPY "%sdk%\templates\%1" %dest% /D /E /C /R /I /K /Y >nul
 echo %dest%
